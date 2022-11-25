914a7074156e9e40cf6ce7ef81ab888415ff4954
HSUHAOBM <hao66bmbm@gmail.com>
2022-11-25 17:30:12 +0800

    feat: 1125
    1125

--- express_socketio/index.js
+++ express_socketio/index.js
@@ -4,18 +4,92 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = 3310
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const session = require("express-session");
+
+const sessionMiddleware = session({
+  secret: "changeit",
+  resave: false,
+  saveUninitialized: false
 });
+app.use(sessionMiddleware);
 
-io.on('connection', (socket) => {
+// middlewares
+const wrap = middleware => (
+  socket, next) => middleware(socket.request, {}, next);
+
+// 空間
+const chat_space = io.of("/chat");
+// add middlewares
+chat_space.use(wrap(sessionMiddleware));
+
+chat_space.on('connection', (socket) => {
+  let session = socket.request.session;
+  let user = session.user;
+  let room = session.room;
+
+  console.log('a user connected');
+  // 加房間
+  socket.join(room);
+
+  // 使用者加入
+  chat_space.to(room).emit('chat message', user + " join the room\r");
+
+  // 訊息傳送
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    chat_space.to(room).emit('chat message', user + " : " + msg );
+  });
+
+  // 離開
+  socket.on('left', () => {
+    console.log('hi')
+    chat_space.socketsLeave(room)
+    chat_space.to(room).emit('chat message', user + " leave the room ");
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+app.get("/",function(req,res){
+  let user = req.session.user;
+  let room = req.session.room;
+  if(user && room){
+    res.redirect("/chatroom")
+  }else{
+    res.render("room");
+  }
+});
+
+app.post("/chat",function(req,res){
+  // 接收表單
+  req.session.user = req.body.user;
+  req.session.room = req.body.room;
+  // create session
+  let user = req.session.user;
+  let room = req.session.room;
+
+  if(user && room){
+    res.redirect("/chatroom")
+  }
+})
+
+//check session test
+app.get("/test",function(req,res){
+  let user = req.session.user;
+  let room = req.session.room;
+  res.send("房" + room + "號 名" + user)
+})
+
+app.get("/chatroom",function(req,res){
+  const room = req.session.room;
+  res.render('index', {room})
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
